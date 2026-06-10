26af09ade81d9da54596f9d283cd000134abcbee
André Luferat <contato@luferat.net>
2026-06-10 13:35:15 -0300

    ok

--- 2026.06.08/exercicio01.py
+++ 2026.06.08/exercicio01.py
@@ -9,7 +9,17 @@ Crie um programa que:
 
 (Os valores devem vir das variáveis.)
 '''
-
+# Armazene o nome de uma pessoa em uma variável.
 name = "Joca"
+
+# Armazene a idade em outra variável.
 age = 65
+
+# Exiba a seguinte mensagem: (usando interpolação)
 print(f"Olá {name}, você tem {age} anos.")
+
+# debug
+# print("-----", type(age), "-----")
+
+# Exiba a seguinte mensagem: (usando concatenação)
+print("Olá " + name + ", você tem " + str(age) + " anos.")
--- 2026.06.08/exercicio02.py
+++ 2026.06.08/exercicio02.py
@@ -17,3 +17,23 @@ Exemplo de saída:
 <class 'bool'>
 ```
 '''
+
+# Um texto
+name = "Joca"
+
+# Um número inteiro
+age = 35
+
+# Um número decimal (float)
+height = 1.78
+
+# Um valor booleano
+is_student = True
+
+# Depois, utilize a função type() para exibir o tipo de cada variável.
+print("name", type(name))
+print("age", type(age))
+print("height", type(height))
+print("is_student", type(is_student))
+
+# --- → DB
